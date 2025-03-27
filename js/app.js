50e7e6f52f6e47ef4e41522c4c5c20ebddd4db3d
Useless-Alex <19alexleo19@gmail.com>
2025-03-27 17:45:42 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Order',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		}
+	],
+	routes: [
+		{
+		path: '/categorias/',
+    	url: 'categorias.html',
+    	name: 'categorias',
+  		}
+
+		
+		
+	],
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
